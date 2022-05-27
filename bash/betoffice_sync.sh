b95afbee1011ee32c74e4d0d1c10c5e5c4c1261d
Andre Winkler <gluehloch@googlemail.com>
2022-05-27 23:48:20 +0200

    Âsync script

--- bash/betoffice_sync.sh
+++ bash/betoffice_sync.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-rsync --delete -avzbe ssh betoffice@tippdiekistebier.de:~/projects/database/vserver ~/tippdiekistebier --backup-dir=~/old
 
-NEWEST_BACKUP=$(ls -t ~/tippdiekistebier/vserver/* | head -1)
-IMPORT_FILE=${NEWEST_BACKUP%.*}
+# boprod@tippdiekistebier.de
+rsync --delete -avzbe ssh mariadb.tdkb2:~/backup ~/tippdiekistebier
 
-echo "Start gunzip of $NEWEST_BACKUP"
-gunzip  $NEWEST_BACKUP
+databaseImport() {
+    echo "Parameter: filename-prefix=$1, user=$2, password=$3, database=$4"
+    echo "----> Start import ..."
 
-echo "Import to mysql ${NEWEST_BACKUP%.*}"
-/bin/mysql -u betofficesu --password=betoffice -D betoffice -h 127.0.0.1 < $IMPORT_FILE
 
+    NEWEST_BACKUP_FILE=$(ls -t ~/tippdiekistebier/backup/$1* | head -1)
+    IMPORT_FILE=${NEWEST_BACKUP_FILE%.*}
+
+    echo "Start gunzip of $NEWEST_BACKUP_FILE"
+    gunzip  "$NEWEST_BACKUP_FILE"
+
+    echo "Import to mysql $NEWEST_BACKUP_FILE%.*"
+    # Virtual Box | betoffice development server
+    # /bin/mysql -u betofficesu --password=betoffice -D betoffice -h 192.168.0.136 < "$IMPORT_FILE"
+    # Docker | betofficedb
+    /bin/mysql -u $2 --password=$3 -D $4 -h 127.0.0.1 < $IMPORT_FILE
+
+    if [ $? -eq 0 ]
+    then
+        echo "----> Import successful."
+    else
+        echo "----> ERROR: $?" 
+    fi
+}
+
+databaseImport 'betoffice' betofficesu betoffice betoffice
+databaseImport 'register' registersu register register
 exit 0
