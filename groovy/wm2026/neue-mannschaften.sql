18659f5a3dec8817c7e8cfc7cf6d89579a657286
Andre Winkler <gluehloch@googlemail.com>
2026-05-25 10:39:15 +0200

    WM 2026 / Neue Mannschaften.

---
+++ groovy/wm2026/neue-mannschaften.sql
@@ -0,0 +1,37 @@
+-- Gruppe C
+-- Haiti
+insert into bo_team(bo_name, bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Haiti', 'Haiti',     null,    1,           null,            5820,          'HTI',        'HTI');
+
+-- Gruppe E
+-- Curacao
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Curaçao', 'Curaçao',   null,    1,           null,            7321,          'CUW',        'CUW');
+
+-- Gruppe H
+-- Kap Verde
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Kap Verde', 'Kapverdische Inseln',   null,    1, null,        6159,          'CPV',        'CPV');
+
+-- Gruppe I
+-- Irak
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Irak', 'Irak',   null,    1, null,        4911,          'IRQ',        'IRQ');
+
+-- Norwegen
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Norwegen', 'Norwegen',   null,    1, null,        1396,          'NOR',        'NOR');
+
+-- Gruppe J
+-- Jordanien
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Norwegen', 'Norwegen',   null,    1, null,        4915,          'JOR',        'JOR');
+
+-- Gruppe K
+-- DR Kongo
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('DR Kongo', 'DR Kongo',   null,    1, null,        4991,          'COD',        'COD');
+
+-- Usbekistan
+insert into bo_team(bo_name,   bo_longname, bo_logo, bo_teamtype, bo_location_ref, bo_openligaid, bo_shortname, bo_xshortname)
+             values('Usbekistan', 'Usbekistan',   null,    1, null,        4910,          'UZB',        'UZB');
