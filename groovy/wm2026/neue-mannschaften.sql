471eff5e0ba96d89918ffaeb7d9043e9cd38dfdf
Andre Winkler <gluehloch@googlemail.com>
2026-05-31 07:51:35 +0200

    Update: WM 2026 / Tipp Teilnehmer anlegen.

--- groovy/wm2026/neue-mannschaften.sql
+++ groovy/wm2026/neue-mannschaften.sql
@@ -44,4 +44,15 @@ INSERT INTO betoffice.bo_teamalias (bo_aliasname, bo_team_ref) VALUES('Bosnien u
 INSERT INTO betoffice.bo_community(bo_shortname, bo_name, bo_year, bo_user_ref, bo_season_ref)
 VALUES('TDKB 2026', 'WM Nordamerika', '2026', 6, 39);
 
+select * from bo_community;
+select * from bo_community_user where bo_community_ref = 38;
 
+INSERT INTO betoffice.bo_community(id, bo_shortname, bo_name, bo_year, bo_user_ref, bo_season_ref)
+VALUES(39, 'TDKB 2026', 'WM Nordamerika', '2026', 6, 39);
+
+insert into bo_community_user(bo_community_ref, bo_user_ref)
+select 39, bo_user_ref from bo_community_user cu where cu.bo_community_ref = 38;
+
+select * from bo_user;
+delete from bo_community_user cu where cu. bo_user_ref = 17290 and bo_community_ref = 39;
+insert into bo_community_user(bo_community_ref, bo_user_ref) values (39, 22);
